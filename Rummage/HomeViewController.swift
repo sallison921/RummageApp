6fc0279488031ab1e8c69f33e2491c062cdc5054
Sydney <sydney.allison@wustl.edu>
2021-11-15 00:54:13 -0600

    home page

--- Rummage/HomeViewController.swift
+++ Rummage/HomeViewController.swift
@@ -59,12 +59,12 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 2
+        return 7
     }
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        var myCGSize = CGSize(width: 350.0, height: 123.0)
@@ -79,15 +79,28 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
-    
-        
-        if(indexPath.section % 2 == 1){
-            cell.backgroundColor = UIColor.blue
-        } else {
-           
-            cell.backgroundColor = UIColor.red
-           
+
+        if indexPath.section == 0 && indexPath.row == 0 {
+            let buttons: UIButton = UIButton(frame: CGRect(x: 0, y: 30, width: view.frame.width/4, height: 30))
+            buttons.backgroundColor = .systemPink
+            buttons.setTitle("Following", for: .normal)
+            cell.addSubview(buttons)
+//            buttons.addTarget(self, action:#selector(addedToFavorites(_:)), for: .touchUpInside)
+            let forYouButton: UIButton = UIButton(frame: CGRect(x: buttons.frame.width + 40, y: 30, width: view.frame.width/4, height: 30))
+            forYouButton.backgroundColor = .systemPink
+            forYouButton.setTitle("For You", for: .normal)
+            cell.addSubview(forYouButton)
         }
+        else {
+            let titleText: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: cell.frame.width, height: 100))
+            titleText.text = "Review!"
+            titleText.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+            titleText.textColor = UIColor.black
+            titleText.textAlignment = .center
+        
+            cell.addSubview(titleText)
+        }
+    
     
         
         return cell
