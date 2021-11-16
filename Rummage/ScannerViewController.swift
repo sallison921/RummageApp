7604f5e67941c86a6df5786b92b675ad229c272a
Nisha Sahgal <nisha.sahgal@wustl.edu>
2021-11-16 16:24:33 -0600

    added code for barcode scanning

---
+++ Rummage/ScannerViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ScannerViewController.swift
+//  BarCodeScanner
+//
+//  Created by 1 on 11/27/20.
+//
+//  scanning code from: https://levelup.gitconnected.com/building-bar-code-scanner-app-in-swift-1de4ab1e1079
+
+import AVFoundation
+import UIKit
+
+@objc protocol ScannerViewDelegate: AnyObject {
+    @objc func didFindScannedText(text: String)
+}
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    @objc public weak var delegate: ScannerViewDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func found(code: String) {
+        print(code)
+        delegate?.didFindScannedText(text: code)
+        self.navigationController?.popViewController(animated: true)
+    }
+}
--- Rummage/ViewController.swift
+++ Rummage/ViewController.swift
@@ -10,19 +10,54 @@ import RealityKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    struct APIResults:Decodable {
+        let products: [Products]
+    }
     
+    struct Products:Decodable {
+        let title: String
+        let images: [String]
+    }
     
+    let scannerViewController = ScannerViewController()
+    var theData:APIResults = APIResults(products: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        //a change here
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        view.backgroundColor = UIColor.systemBlue
+        scanBarButton.addTarget(self, action: #selector(scanBarTapped), for: .touchUpInside)
+        scannerViewController.delegate = self
     }
 }
+
+extension ViewController: ScannerViewDelegate {
+    func didFindScannedText(text: String) {
+        scanTextField.text = text
+        fetchDataForCollectionView(query: text)
+    }
+    
+    func fetchDataForCollectionView(query: String) {
+        let itemTitle = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: "https://api.barcodelookup.com/v3/products?barcode=\(itemTitle!)&formatted=y&key=fpgm6wv6vm8tp3wf8hwupzhi3gkpq2")
+        let data = try! Data(contentsOf: url!)
+        theData = try! JSONDecoder().decode(APIResults.self, from:data)
+        let result = theData.products[0]
+        productLabel.text = "Product: \(result.title)"
+        cacheImages(item: result)
+    }
+    
+    func cacheImages(item: Products) {
+        let link = item.images[0]
+        
+        if (link != ""){
+            let url = URL(string: link)
+            let data = try? Data(contentsOf: url!)
+            let image = UIImage(data: data!)
+            productImage.image = image
+        }
+        else {
+            print("no image")
+        }
+    }
+}
+
