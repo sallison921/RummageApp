e26b6e67fed6247986fad2fc394993d74f98e572
Sydney <sydney.allison@wustl.edu>
2021-12-05 20:47:09 -0600

    ah

---
+++ Rummage/LogInViewController.swift
@@ -0,0 +1,69 @@
+//
+//  RegisterViewController.swift
+//  Rummage
+//
+//  Created by Sydney A on 12/5/21.
+//
+
+import UIKit
+import Firebase
+class LogInViewController: UIViewController {
+   
+
+//login stuff
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var pwSignin: UITextField!
+    @IBOutlet weak var emailSignin: UITextField!
+    
+    @IBOutlet weak var errorFound: UILabel!
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        pwSignin.isSecureTextEntry = true
+    }
+    
+
+    
+    @IBAction func loginClicked(_ sender: Any) {
+       logIn()
+    }
+    
+    func logIn(){
+        Auth.auth().signIn(withEmail: emailSignin.text ?? "", password: pwSignin.text ?? "") {(user, error) in
+            if user != nil {
+                print("User Has Sign In")
+                
+                
+                //https://fluffy.es/how-to-transition-from-login-screen-to-tab-bar-controller/ got help with changing root controller (same as app delegate)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let mainTabBarController = storyboard.instantiateViewController(identifier: "mainTab")
+                (UIApplication.shared.delegate as? AppDelegate)?.changeRootViewController(mainTabBarController)
+            }
+            
+            
+            if error != nil {
+                //print(":(",error ?? "")
+                print(error.debugDescription)
+                self.errorFound.numberOfLines = 3
+                self.errorFound.text = "There was an error logging in, try again"
+            }
+        }
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
+
+
+
+
