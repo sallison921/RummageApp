44b651b4e5e632f96d2a011d17431b7390ce2ebd
tessjenkins <jenkinstess@wustl.edu>
2021-12-06 22:21:23 -0600

    try

--- Rummage/GetRecipesViewController.swift
+++ Rummage/GetRecipesViewController.swift
@@ -82,34 +82,34 @@ class GetRecipesViewController: UIViewController, UITableViewDelegate, UITableVi
         let strDrinkThumb: String //string url of image of drink
         let strIngredient1: String //ingredient 1
         let strIngredient2: String //ingredient 2
-        let strIngredient3: String //ingredient 3
-        let strIngredient4: String //ingredient 4
-        let strIngredient5: String //ingredient 5
-        let strIngredient6: String //ingredient 6
-        let strIngredient7: String //ingredient 7
-        let strIngredient8: String //ingredient 8
-        let strIngredient9: String //ingredient 9
-        let strIngredient10: String //ingredient 10
-        let strIngredient11: String //ingredient 11
-        let strIngredient12: String //ingredient 12
-        let strIngredient13: String //ingredient 13
-        let strIngredient14: String //ingredient 14
-        let strIngredient15: String //ingredient 15
+        let strIngredient3: String! //ingredient 3
+        let strIngredient4: String! //ingredient 4
+        let strIngredient5: String! //ingredient 5
+        let strIngredient6: String! //ingredient 6
+        let strIngredient7: String! //ingredient 7
+        let strIngredient8: String! //ingredient 8
+        let strIngredient9: String!//ingredient 9
+        let strIngredient10: String! //ingredient 10
+        let strIngredient11: String! //ingredient 11
+        let strIngredient12: String! //ingredient 12
+        let strIngredient13: String! //ingredient 13
+        let strIngredient14: String! //ingredient 14
+        let strIngredient15: String! //ingredient 15
         let strMeasure1: String //measurement of ingredient 1
         let strMeasure2: String //measurement of ingredient 2
-        let strMeasure3: String //measurement of ingredient 3
-        let strMeasure4: String //measurement of ingredient 4
-        let strMeasure5: String //measurement of ingredient 5
-        let strMeasure6: String //measurement of ingredient 6
-        let strMeasure7: String //measurement of ingredient 7
-        let strMeasure8: String //measurement of ingredient 8
-        let strMeasure9: String //measurement of ingredient 9
-        let strMeasure10: String //measurement of ingredient 10
-        let strMeasure11: String //measurement of ingredient 11
-        let strMeasure12: String //measurement of ingredient 12
-        let strMeasure13: String //measurement of ingredient 13
-        let strMeasure14: String //measurement of ingredient 14
-        let strMeasure15: String //measurement of ingredient 15
+        let strMeasure3: String! //measurement of ingredient 3
+        let strMeasure4: String! //measurement of ingredient 4
+        let strMeasure5: String! //measurement of ingredient 5
+        let strMeasure6: String! //measurement of ingredient 6
+        let strMeasure7: String! //measurement of ingredient 7
+        let strMeasure8: String! //measurement of ingredient 8
+        let strMeasure9: String! //measurement of ingredient 9
+        let strMeasure10: String! //measurement of ingredient 10
+        let strMeasure11: String! //measurement of ingredient 11
+        let strMeasure12: String! //measurement of ingredient 12
+        let strMeasure13: String! //measurement of ingredient 13
+        let strMeasure14: String! //measurement of ingredient 14
+        let strMeasure15: String! //measurement of ingredient 15
     }
     
 //    struct specRecipeInfo:Decodable{
